8a91ce5edf531dd667d29b711d92a347f46130a6
YuJin Kim <axionbuster@gmail.com>
2024-02-12 15:24:25 -0800

    Remove hypot from disk-rectangle intersection detection
    The hypot calls were estimated to be O(n) where n is the number of particles in the previous implementation.
    
    I will add in the tests soon when I find the time to do it.

--- dyn/circle.h
+++ dyn/circle.h
@@ -5,6 +5,7 @@
 /// @brief Represent a circle by the center and radius.
 
 #include <complex>
+#include <numbers>
 
 namespace dyn {
 
@@ -22,37 +23,50 @@ template <typename F = float> struct Circle : public std::complex<F> {
       : std::complex<F>(center), radius(radius) {}
 };
 
+/// @brief Decide whether at least one intersection (point) exists between the
+/// area of the disk centered at the origin with the given radius and the area
+/// of the given rectangle.
+/// @param ll The less-less corner of the rectangle.
+/// @param gg The greater-greater corner of the rectangle.
+template <typename F = float>
+bool origin_disk_rect_isct(F radius, std::complex<F> ll,
+                           std::complex<F> gg) noexcept {
+  // sin(45 deg) [also cos(45 deg)].
+  constexpr F sc45 = std::numbers::sqrt2_v<F> / F(2);
+
+  // The rectangle's four corners are in four different quadrants.
+  if (std::signbit(ll.real()) != std::signbit(gg.real()) &&
+      std::signbit(ll.imag()) != std::signbit(gg.imag()))
+    return true;
+
+  // Decide whether the given rectangle and a square centered at (0,0) having a
+  // "radius" (half side length) of `radius` (thus a full side length of twice
+  // the `radius`).
+  auto recsqisct = [&]() {
+    return -radius < gg.real() && ll.real() < radius && -radius < gg.imag() &&
+           gg.imag() < radius;
+  };
+
+  // The rectangle does not touch the bounding square of the circle.
+  if (!recsqisct())
+    return false;
+
+  // The rectangle touches the interior square of the circle.
+  radius *= sc45;
+  if (recsqisct())
+    return true;
+
+  // They don't touch.
+  return false;
+}
+
 /// @brief Decide whether at least one intersection (point) exists between a
 /// circular disk and a rectangle (degenerate cases are unspecified).
 template <typename F = float>
-constexpr bool disk_rect_intersect(Circle<F> circ, std::complex<F> ll,
-                                   std::complex<F> gg) {
+bool disk_rect_intersect(Circle<F> circ, std::complex<F> ll,
+                         std::complex<F> gg) noexcept {
   ll -= circ, gg -= circ;
-  auto rad = circ.radius;
-
-  // Suppose that the circle and the rectangle intersect, then:
-
-  // 1. The circle's bounding square and the rectangle must intersect.
-  if (ll.real() > rad || ll.imag() > rad || gg.real() < -rad ||
-      gg.imag() < -rad)
-    // (They don't. -> No intersection.)
-    return false;
-  // 2. Otherwise, if either side of the rectangle cut through a coordinate
-  // axis then they intersect.
-  if (std::signbit(ll.real()) != std::signbit(gg.real()) ||
-      std::signbit(ll.imag()) != std::signbit(gg.imag()))
-    // (It does. -> Intersection.)
-    return true;
-  // 3. Lastly, otherwise, if any of the four corners are in the disk bounded
-  // by the circle then the circle and the rectangle intersect.
-  F cc[4] = {std::abs(ll), std::abs(gg),
-             std::abs(std::complex<F>{ll.real(), gg.imag()}),
-             std::abs(std::complex<F>{ll.imag(), gg.real()})};
-  for (auto &&c : cc)
-    if (c < rad)
-      return true;
-  // No other intersecting cases exist.
-  return false;
+  return origin_disk_rect_isct(circ.radius, ll, gg);
 }
 
 } // namespace dyn
