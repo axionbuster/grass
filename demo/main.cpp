a9aee9ca6d2f1dc37c836a148b97a9b2c560e881
YuJin Kim <axionbuster@gmail.com>
2024-02-13 08:44:35 -0800

    Revert to using Yoshida
    From the CI/CD reports I know that the new syntax works so I revert to old behavior as intended.

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,7 +8,6 @@
 
 #include <circle.h>
 #include <halton.h>
-#include <verlet.h>
 
 #include "Table.h"
 #include "irhall.h"
@@ -46,7 +45,7 @@ static int do_main() {
   InitWindow(600, 600, "Basic 1,000 particle demo (click to add particles)");
 
   // The physical table (store particles, etc.); backup.
-  Table<dyn::Verlet<float>> table, table0;
+  Table table, table0;
 
   // Make the mystical figure-8 shape below work at first.
   // (This G value is too large in most cases, so lower it once the user starts
