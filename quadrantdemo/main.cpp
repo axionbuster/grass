a944862a0a68f3f4155102932b763e5ab09aa128
YuJin Kim <axionbuster@gmail.com>
2024-02-16 14:49:09 -0800

    Add colors and filling animation to Morton demo

--- quadrantdemo/main.cpp
+++ quadrantdemo/main.cpp
@@ -1,30 +1,31 @@
 #include <algorithm>
 #include <array>
+#include <deque>
 #include <morton.h>
 #include <random>
 #include <raylib.h>
 #include <vector>
 
-int do_main() {
-  long constexpr SEED = 1234, N_PARTICLES = 100, DIVISOR = 25,
-                 C_QUOTIENT = N_PARTICLES / DIVISOR + !!(N_PARTICLES % DIVISOR);
-  float constexpr RADIUS = 0.00390625f;
+std::array<float, 3> hsl2rgb(std::array<float, 3> hsl) {
+  auto l = hsl[2], a = hsl[1] * std::min(l, 1.0f - l);
+  auto min3 = [](float a, float b, float c) {
+    return std::min(a, std::min(b, c));
+  };
+  auto f = [l, a, &hsl, &min3](float n) {
+    auto k = std::remainder(n + hsl[0] * 12.0f, 12.0f);
+    return l - a * std::max(-1.0f, min3(k - 3.0f, 9.0f - k, 1.0f));
+  };
+  return {f(0.0f), f(8.0f), f(4.0f)};
+}
 
-  std::array<Color, C_QUOTIENT> colors{};
+int do_main() {
+  long constexpr SEED = 5432, N_PARTICLES = 10000,
+                 MAX_N_QUEUE = N_PARTICLES / 4;
+  float constexpr RADIUS = 0.00390625f, SATURATION = 1.0f, LIGHTNESS = 0.66f;
+  uint64_t constexpr MASK = 0xffff'0000'0000'0000;
+
   std::mt19937 rng(SEED);
   std::uniform_real_distribution<float> udist;
-  // Generate a fixed palette of colors.
-  for (auto &&c : colors) {
-    struct {
-      float r, g, b;
-      [[nodiscard]] constexpr Color color() const {
-        // r, g, b, a.
-        return {(unsigned char)(255.0f * r), (unsigned char)(255.0f * g),
-                (unsigned char)(255.0f * b), 255};
-      }
-    } a{udist(rng), udist(rng), udist(rng)};
-    c = a.color();
-  }
 
   // Generate particles and then sort them by Morton order.
   std::vector<std::complex<float>> pp;
@@ -35,48 +36,44 @@ int do_main() {
     return dyn::morton32(p) < dyn::morton32(q);
   });
 
-  struct Box {
-    std::complex<float> ll, gg;
-    [[nodiscard]] constexpr Rectangle rectangle() const {
-      auto diff = gg - ll;
-      return {ll.real(), ll.imag(), diff.real(), diff.imag()};
-    }
-  };
-  std::vector<Box> boxes;
-  for (long q = 0; q < C_QUOTIENT; q++) {
-    auto ill = q * DIVISOR, igg = std::min((q + 1) * DIVISOR, N_PARTICLES) - 1L;
-    boxes.emplace_back(pp[ill], pp[igg]);
-  }
-
   InitWindow(600, 600, "Q");
-  SetTargetFPS(90);
+  SetTargetFPS(60);
   Camera2D cam{};
   cam.zoom = 600.0f;
+  long q{};
+  uint64_t prefix{};
+  std::deque<Color> colors;
   while (!WindowShouldClose()) {
     BeginDrawing();
     BeginMode2D(cam);
     {
       ClearBackground(BLACK);
       // Color and then draw the particles.
-      for (long i = 0; i < N_PARTICLES; i++) {
-        auto color = colors[i / DIVISOR];
-        auto particle = pp[i];
-        DrawCircleV({particle.real(), particle.imag()}, RADIUS, color);
+      auto particle = pp[q];
+      auto jpfx = dyn::morton32(particle) & MASK;
+      if (jpfx != prefix) {
+        auto hue = udist(rng);
+        auto rgb = hsl2rgb({hue, SATURATION, LIGHTNESS});
+        colors.emplace_front((unsigned char)(255.0f * rgb[0]),
+                             (unsigned char)(255.0f * rgb[1]),
+                             (unsigned char)(255.0f * rgb[2]), 255);
+        if (colors.size() > MAX_N_QUEUE)
+          colors.pop_back();
+      } else {
+        colors.push_front(colors.front());
+        if (colors.size() > MAX_N_QUEUE)
+          colors.pop_back();
       }
-      // Draw the bounding boxes.
-      for (long i = 0; i < boxes.size(); i++) {
-        auto color = colors[i];
-        auto rectangle = boxes[i].rectangle();
-        if (rectangle.width < 0) {
-          rectangle.x += rectangle.width;
-          rectangle.width = std::abs(rectangle.width);
-        }
-        if (rectangle.height < 0) {
-          rectangle.y += rectangle.height;
-          rectangle.height = std::abs(rectangle.height);
-        }
-        DrawRectangleLinesEx(rectangle, RADIUS, color);
+      prefix = jpfx;
+      for (long n = 0; n < MAX_N_QUEUE; n++) {
+        auto i = q - n;
+        auto j = i < 0 ? i + N_PARTICLES : i;
+        auto p = pp[j];
+        auto c = n < colors.size() ? colors[n] : BLACK;
+        c = Fade(c, 1.0f / (1.0f + std::pow(float(n), 0.25f)));
+        DrawCircleV({p.real(), p.imag()}, RADIUS, c);
       }
+      q = (q + 1) % N_PARTICLES;
     }
     EndMode2D();
     EndDrawing();
