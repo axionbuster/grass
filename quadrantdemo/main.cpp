554e3123901874d9f44c6b075a587f611cd45b8e
YuJin Kim <axionbuster@gmail.com>
2024-02-17 09:00:24 -0800

    Demonstrate four quadrants

--- quadrantdemo/main.cpp
+++ quadrantdemo/main.cpp
@@ -7,51 +7,69 @@
 #include <vector>
 
 std::array<float, 3> hsl2rgb(std::array<float, 3> hsl) {
+  // From Wikipedia. Don't ask me what these variables stand for or how this
+  // works, but you can correspond these letters and notation to the math.
   auto l = hsl[2], a = hsl[1] * std::min(l, 1.0f - l);
-  auto min3 = [](float a, float b, float c) {
-    return std::min(a, std::min(b, c));
-  };
-  auto f = [l, a, &hsl, &min3](float n) {
+  auto f = [l, a, &hsl](auto n) {
     auto k = std::remainder(n + hsl[0] * 12.0f, 12.0f);
-    return l - a * std::max(-1.0f, min3(k - 3.0f, 9.0f - k, 1.0f));
+    auto m = std::min(k - 3.0f, std::min(9.0f - k, 1.0f));
+    return l - a * std::max(-1.0f, m);
   };
   return {f(0.0f), f(8.0f), f(4.0f)};
 }
 
-int do_main() {
-  long constexpr SEED = 5432, N_PARTICLES = 10000,
-                 MAX_N_QUEUE = N_PARTICLES / 4;
-  float constexpr RADIUS = 0.00390625f, SATURATION = 1.0f, LIGHTNESS = 0.66f;
+/// @brief Do main loop. Return `true` if to be shown again.
+bool show() {
+  long constexpr N_PARTICLES = 10000, MAX_N_QUEUE = N_PARTICLES / 4;
+  float constexpr RADIUS = 0.00390625f, SATURATION = 0.75f, LIGHTNESS = 0.66f;
   uint64_t constexpr MASK = 0xffff'0000'0000'0000;
 
-  std::mt19937 rng(SEED);
+  std::random_device seed;
+  std::mt19937 rng(seed());
   std::uniform_real_distribution<float> udist;
 
   // Generate particles and then sort them by Morton order.
   std::vector<std::complex<float>> pp;
   for (long i = 0; i < N_PARTICLES; i++) {
-    pp.emplace_back(udist(rng), udist(rng));
+    auto point = [&rng, &udist]() { return 2.0f * udist(rng) - 1.0f; };
+    pp.emplace_back(point(), point());
   }
-  std::sort(pp.begin(), pp.end(), [](auto const &p, auto const &q) {
-    return dyn::morton32(p) < dyn::morton32(q);
-  });
+  std::ranges::sort(pp.begin(), pp.end(), {}, dyn::morton32);
 
-  InitWindow(600, 600, "Q");
-  SetTargetFPS(60);
+  // Let center of window point to world origin.
+  auto scw = float(GetScreenWidth()), sch = float(GetScreenHeight());
+  auto scs = std::min(scw, sch) * 0.5f;
   Camera2D cam{};
-  cam.zoom = 600.0f;
+  cam.zoom = scs;
+  cam.offset = {scs, scs};
+
+  // Let the drawing begin at the particle at index q.
   long q{};
+  // Morton prefix (same color or different color?) See also: MASK.
   uint64_t prefix{};
   std::deque<Color> colors;
   while (!WindowShouldClose()) {
     BeginDrawing();
+    // If `R` pressed, that means to reset.
+    // Return while returning `true` (do this again).
+    if (IsKeyPressed(KEY_R))
+      // Return after `BeginDrawing` to avoid freeze.
+      // (This also means to put `EndDrawing` here as well.)
+      return EndDrawing(), true;
     BeginMode2D(cam);
     {
       ClearBackground(BLACK);
       // Color and then draw the particles.
       auto particle = pp[q];
-      auto jpfx = dyn::morton32(particle) & MASK;
-      if (jpfx != prefix) {
+      // Compare prefix with previous particle to decide new color.
+      auto pfx = dyn::morton32(particle) & MASK;
+      if (pfx == prefix) {
+        // Same quadrant, old color.
+        colors.push_front(colors.front());
+        if (colors.size() > MAX_N_QUEUE)
+          colors.pop_back();
+      } else {
+        // New quadrant, new color.
         auto hue = udist(rng);
         auto rgb = hsl2rgb({hue, SATURATION, LIGHTNESS});
         colors.emplace_front((unsigned char)(255.0f * rgb[0]),
@@ -59,18 +77,20 @@ int do_main() {
                              (unsigned char)(255.0f * rgb[2]), 255);
         if (colors.size() > MAX_N_QUEUE)
           colors.pop_back();
-      } else {
-        colors.push_front(colors.front());
-        if (colors.size() > MAX_N_QUEUE)
-          colors.pop_back();
       }
-      prefix = jpfx;
+      prefix = pfx;
+      // Draw particle at index q but also the trailing particles (n).
       for (long n = 0; n < MAX_N_QUEUE; n++) {
+        // Compute actual index (j) of the particle (p) as a function of index q
+        // and trail number n.
         auto i = q - n;
         auto j = i < 0 ? i + N_PARTICLES : i;
         auto p = pp[j];
+        // Manipulate color (fade out).
         auto c = n < colors.size() ? colors[n] : BLACK;
-        c = Fade(c, 1.0f / (1.0f + std::pow(float(n), 0.25f)));
+        auto ease_cubic = [](float a) { return 1.0f - a * a * a; };
+        c = Fade(c, ease_cubic(float(n) / float(MAX_N_QUEUE)));
+        // Yup. Draw it.
         DrawCircleV({p.real(), p.imag()}, RADIUS, c);
       }
       q = (q + 1) % N_PARTICLES;
@@ -78,8 +98,17 @@ int do_main() {
     EndMode2D();
     EndDrawing();
   }
-  CloseWindow();
 
+  // Don't re-run (probably never reached).
+  return false;
+}
+
+int do_main() {
+  InitWindow(600, 600, "Q");
+  SetTargetFPS(60);
+  while (show())
+    ;
+  CloseWindow();
   return 0;
 }
 
