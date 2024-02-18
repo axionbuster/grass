540690c4b429d93f2dce688b393e2feb8d6e4997
YuJin Kim <axionbuster@gmail.com>
2024-02-18 07:14:07 -0800

    Cut dead function morton32; add tests

--- dyn/morton.h
+++ dyn/morton.h
@@ -10,35 +10,34 @@
 namespace dyn {
 
 namespace morton::detail {
-constexpr uint32_t order32(float x) {
-  // Due to Tropf (2021).
-  auto sgn = uint32_t(1) << 31, i = std::bit_cast<uint32_t>(x);
-  return (i & sgn) ? ~i : (i | sgn);
-}
 
+/// @brief Interleave the bits of two 32-bit words (re, im) so that the word
+/// im is placed at the odd-numbered bits (including the most significant bit)
+/// while re is placed at the even-numbered bits (including the least
+/// significant bit). Bits are numbered from 0 (LSB) to 61 (MSB) inclusive.
 constexpr uint64_t interleave32(uint32_t re, uint32_t im) {
   // Modification of "Interleave by Binary Magic Numbers"
   // (http://graphics.stanford.edu/~seander/bithacks.html#InterleaveBMN)
   struct Help {
-    // 0101..., 00110011...,
-    // 00001111..., 0000000011111111...,
-    // including until the entire word is like 0...0_1...1_0...0_1...1
-    // (In `H` below, the above is done in reverse order.)
-    uint64_t mask;
-    // Number of bits to shift.
-    uint64_t shift;
+    uint64_t mask, shift;
   };
 
   // 32-bit words. Hence, 5.
+  // 0101..., 00110011...,
+  // 00001111..., 0000000011111111...,
+  // including until the entire word is like 0...0_1...1_0...0_1...1.
+  // (Reverse order, however.)
   std::array<Help, 5> constexpr H{
       Help{0x0000ffff0000ffff, 16}, Help{0x00ff00ff00ff00ff, 8},
       Help{0x0f0f0f0f0f0f0f0f, 4}, Help{0x3333333333333333, 2},
       Help{0x5555555555555555, 1}};
 
+  // Zero-extend each word.
   std::array<uint64_t, 2> W{re, im};
   for (auto &&w : W)
+    // Spread the bits out with zeros in between.
+    // (ex) w = 0b1011 -> 0b01'00'01'01
     for (auto &&h : H)
-      // "Duplicate" the bits (shift-then-OR) and then mask result.
       w = (w | (w << h.shift)) & h.mask;
 
   // Imaginary first.
@@ -46,19 +45,19 @@ constexpr uint64_t interleave32(uint32_t re, uint32_t im) {
 }
 } // namespace morton::detail
 
-constexpr uint64_t morton32(std::complex<float> xy) {
-  using namespace morton::detail;
-  return interleave32(order32(xy.real()), order32(xy.imag()));
-}
-
+/// @brief Compute the Morton (Z) code of a complex number xy assuming a squared
+/// grid by pre-multiplying the factor `Precision` to each component of xy.
+/// If either component is not a finite number after the scaling, the result
+/// is {} (no value).
 template <uint32_t Precision = 512>
 std::optional<uint64_t> fixedmorton32(std::complex<float> xy) {
   xy *= float(Precision);
   if (std::abs(xy.real()) < float(INT32_MAX) &&
       std::abs(xy.imag()) < float(INT32_MAX)) {
+    // unsigned.
     auto sgn = 0x8000'0000u;
-    auto x = std::bit_cast<uint32_t>(int32_t(xy.real())) ^ sgn;
-    auto y = std::bit_cast<uint32_t>(int32_t(xy.imag())) ^ sgn;
+    auto x = int32_t(xy.real()) ^ sgn; // promoted to unsigned.
+    auto y = int32_t(xy.imag()) ^ sgn; // (ditto).
     return morton::detail::interleave32(x, y);
   }
   return {};
--- tests/morton_test.cpp
+++ tests/morton_test.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include <algorithm>
+#include <array>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
@@ -12,3 +14,39 @@ TEST(MortonDetail, Interleave0) {
   ASSERT_EQ(e, g) << "hex (e) = 0x" << std::hex << std::setw(16) << e
                   << "\nhex (g) = 0x" << std::setw(16) << g;
 }
+
+TEST(Morton, Fixed512_0) {
+  auto X = 4194304; // INT32_MAX / 512
+  std::complex<float> a{float(X), float(X)};
+  auto b = dyn::fixedmorton32<512>(a);
+  ASSERT_FALSE(b.has_value());
+}
+
+TEST(Morton, Fixed512_1) {
+  auto X = 12345;
+  std::complex<float> a{float(X), float(X)};
+  auto b = dyn::fixedmorton32<512>(a);
+  ASSERT_TRUE(b.has_value());
+}
+
+TEST(Morton, Fixed512_2) {
+  typedef std::complex<float> C;
+  // Already sorted in Z-order (Morton order; no change expected).
+  std::array<C, 4> z_in{C{-12.0f, -11.0f}, C{24.0f, -3.23f}, C{-11.0f, 4.8f},
+                        C{1.2f, 3.4f}};
+  auto z_out{z_in};
+  std::ranges::sort(z_out.begin(), z_out.end(), {}, dyn::fixedmorton32<512>);
+  ASSERT_EQ(z_in, z_out);
+}
+
+TEST(Morton, Fixed512_3) {
+  typedef std::complex<float> C;
+  // In incorrect Z-order (must sort).
+  std::array<C, 2> z_in{C{11.0f, 3.3f}, {-2.0f, 0.2f}};
+  std::array<C, 2> z_expect{C{-2.0f, 0.2f}, {11.0f, 3.3f}};
+  auto z_out{z_in};
+  std::ranges::sort(z_out.begin(), z_out.end(), {}, dyn::fixedmorton32<512>);
+  ASSERT_EQ(z_expect, z_out);
+}
+
+// Notes: Order of NaN and infinite values are unspecified.
