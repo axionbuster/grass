b54563d88587f2d7a16f4258e8c013d610a5e5f4
YuJin Kim <axionbuster@gmail.com>
2024-02-20 14:32:57 -0800

    barnes_hut.h: Simplify; remove dfs()
    Dfs approach considered not necessary.

--- dyn/barnes_hut.h
+++ dyn/barnes_hut.h
@@ -2,10 +2,6 @@
 #define GRASS_BARNES_HUT_H
 
 #include <algorithm>
-#include <complex>
-#include <concepts>
-#include <ranges>
-#include <vector>
 
 namespace dyn::bh32 {
 
@@ -16,21 +12,10 @@ void group(auto begin, auto const end, auto &&z, auto &&node) {
   while (begin != end) {
     auto [first, last] = std::ranges::equal_range(begin, end, z(*begin), {}, z);
     if (first != last)
-      node(first, last), begin = last;
+      node(first, begin = last);
     else
       ++begin;
   }
-}
-
-void dfs(auto begin, auto const end, auto &&far, auto &&react) {
-  if (begin != end)
-    do
-      if (far(*begin))
-        react(*begin);
-      else
-        dfs(begin->kids.begin(), begin->kids.end(), std::forward(far),
-            std::forward(react));
-    while (++begin != end);
 }
 
 } // namespace dyn::bh32
