95f01b732ab8a6b1990b01fd2afa83acf2a133cc
axionbuster <axionbuster@gmail.com>
2024-03-07 20:25:55 -0800

    Fix Windows build issue

--- demo/env.h
+++ demo/env.h
@@ -8,11 +8,28 @@
 
 namespace env {
 
-std::optional<std::string> get(std::string_view sv) {
-  if (auto s = std::getenv(sv.cbegin()))
+std::optional<std::string> get(char const *sv) {
+#ifndef _WIN32
+  // On non-Windows platforms, s may exist as an empty character if the
+  // environment variable exists but the value is empty. In this case, the first
+  // branch below is taken.
+  if (char const *s = std::getenv(sv))
     return s;
   else
     return {};
+#else
+  char *s;
+  size_t n;
+  // On Windows, _dupenv_s is the Microsoft-recommended way of reading an
+  // environment variable. When the variable isn't found, the error (e) is 0 and
+  // the count (n) is also 0 (also, the buffer [s] is set to NULL).
+  if (auto e = _dupenv_s(&s, &n, sv); e || !n)
+    return {};
+  std::string t{s, n};
+  // According to Microsoft, the buffer s must be freed using a call to `free`.
+  std::free(s);
+  return t;
+#endif
 }
 
 } // namespace env
